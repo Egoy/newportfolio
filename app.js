f404a6f2625e9b258bef6943555d3d505a054f24
Egoy <cangcoder@gmail.com>
2022-08-18 03:10:17 +0800

    Finished coding light mode of the page

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ for (let i=0; i<list.length; i++){
         list[i].className = 'list active'
     }
 }
+
+let offButton = document.getElementById('off-button')
+let onButton = document.getElementById('on-button')
+let spanText = document.getElementById('text--logo')
+
+offButton.addEventListener('click', () => {
+    offButton.classList.remove('active')
+    offButton.classList.add('inactive')
+    onButton.classList.add('active')
+    onButton.classList.remove('inactive')
+    document.body.classList.toggle('dMode')
+    spanText.classList.add('dMode1')
+})
+onButton.addEventListener('click', () => {
+    onButton.classList.remove('active')
+    onButton.classList.add('inactive')
+    offButton.classList.add('active')
+    offButton.classList.remove('inactive')
+    document.body.classList.toggle('dMode')
+})
